0e327e6796113b537c98e007ed34c0107b0a968a
samuel-abilawon <sam@atafrica.org>
2023-03-09 11:09:39 -0800

    changed port

--- src/index.js
+++ src/index.js
@@ -17,12 +17,13 @@ app.use(cors());
 app.use(router);
 //--using bodyParser to parse JSON bodies into JS objects--//
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3030;
 
 app.get('/', (req, res) => {
     res.send("Everything is working fine!");
 }); 
 
 //--starting the server--//
-app.listen(3001, () => {
- console.log('listening on port 3001');
+app.listen(PORT, () => {
+ console.log(`listening on port ${PORT}`);
 });
